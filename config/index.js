2d5bdd7ac00d7fc201e66dc7c8b6b6b66be67aee
Huan Nguyen <huannv@solazu.com>
2024-06-05 15:53:17 +0700

    config cloud

--- config/index.js
+++ config/index.js
@@ -1,8 +1,9 @@
-const amqpUrl = "amqp://127.0.0.1:5672" // using docker
+// const amqpUrl = "amqp://127.0.0.1:5672" // using docker
+const amqpUrlCloud = "amqps://pjrzgkug:3aVtADCfGBZ5ODAxc9K0dOc9rjYeow_R@shrimp.rmq.cloudamqp.com/pjrzgkug"
 module.exports = {
   NODE_PORT: 3343,
   RBMQ: {
-    SERVER: amqpUrl,
+    SERVER: amqpUrlCloud,
     EXCHANGE: {
       C_VALIDATE_JSON: 'validateJSON',
       T_VALIDATE_JSON: 'topic_validateJSON'
