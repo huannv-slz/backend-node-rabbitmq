8004f4b2293ae52b6c08f738c12f5098c35bf0b6
Huan Nguyen <huannv@solazu.com>
2024-06-05 15:01:28 +0700

    update consumer

--- direct/consumer.js
+++ direct/consumer.js
@@ -11,15 +11,15 @@ function listen() {
   consumer = amqp.connect(CONFIG.RBMQ.SERVER);
   consumer.then(function(conn) {
       return conn.createChannel().then(function(ch) {
+        console.log("######## Consum ok")
         ch.assertExchange(CONFIG.RBMQ.ROUTING.C_VALIDATE_JSON, "direct", {durable: true, autoDelete: false});
           //one-to-one messaging uses the default exchange, where queue name is the routing key
           /**
            * >>>> `exclusive` set to false; so that if consumer is down, the incoming message are in queue without deleting <<<<
            */
           ch.assertQueue(CONFIG.RBMQ.ROUTING.C_VALIDATE_JSON, {durable: true, autoDelete: false, exclusive: false});
-          ch.consume(CONFIG.RBMQ.ROUTING.C_VALIDATE_JSON, function(message) {
-              //callback funtion on receiving messages
-              // console.log(message.content.toString());
+          ch.consume(CONFIG.RBMQ.ROUTING.T_VALIDATE_JSON, function(message) {
+            console.log(message.content.toString());
           }, {noAck: true});
       });
   }).then(null, function(err) {
@@ -31,6 +31,6 @@ function listen() {
 function relisten() {
   consumer.then(function(conn) {
       conn.close();
-  }); 
+  });
   setTimeout(listen, 5000);
 }
